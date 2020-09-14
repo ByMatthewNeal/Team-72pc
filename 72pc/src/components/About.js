fcedbdcbb80b5c65f568a68d1ce6123f12544a66
cjoynerw <cjoynerw@gmail.com>
2020-09-14 18:02:04 -0500

    added navbar with some styling

--- 72pc/src/components/About.js
+++ 72pc/src/components/About.js
@@ -4,7 +4,14 @@ function About() {
     return(
         <div className='about'>
             <h1>About 72PC</h1>
-            <p>guy</p>
+            <p>In 2007 the initial iteration of 72 Pin Connector was born. Originally, we started as a few college buddies and a podcast discussing retro games however 
+            the podcast quickly died down. Then came Rocket League. Adam was playing Rocket League with Josh and some dudes in the "Casual Gaming" discord. 
+            Adam bridged the gap initally between Causal Gaming and 72PC. Over time there was a lot of cross polination between Casual Gaming and 72PC. Josh, RS, and 
+            Adam decided to play RL kind of competitively so they played with the banner of 72PC. As that was going on, Josh also joined the podcasting group. 
+            A few months after this point the 72PC discord and Casual gaming discords came together. A few months later, Josh, RS, and Adam decided to step back from 
+            RL and Josh decided to try coaching the team of BP, DiDi, and Vanr. This was when we really started to take our current form. 
+            There have been roster shifts, another merge (with Dauntless), and some other small things. But that's how we got started and today we compete in RLCS X as 
+            well as other major tournaments in Rocket League and have a discord community of over 1,000 members.</p>
         </div>
     )
 }
--- 72pc/src/components/Navbar.js
+++ 72pc/src/components/Navbar.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import '../styles/navbar.scss';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return(
         <div className='navbar'>
-            <h1>Navbar 72PC</h1>
-            <p>guy</p>
+            <ul className='nav-list'>
+                <li><Link to='/' className='link'>Home</Link></li>
+                <li><Link to='/about' className='link'>About</Link></li>
+                <li><Link to='/events' className='link'>Events</Link></li>
+                <li><Link to='/news' className='link'>News</Link></li>
+                <li><Link to='/team' className='link'>Team</Link></li>
+                <li><a href='https://akquire.ca/72pinconnector/' target='blank' className='link'>Merch</a></li>
+            </ul>
         </div>
     )
 }
